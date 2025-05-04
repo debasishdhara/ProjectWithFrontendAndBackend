93bb3204a5fadda4651032fbe60a253f2a324f28
Debasish <debasish@ascentspark.com>
2025-05-04 12:30:23 +0530

    fixed link

--- resources/src/App.tsx
+++ resources/src/App.tsx
@@ -25,10 +25,10 @@ class App extends Component<{}, AppState> {
     return (
       <>
         <div>
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <a href="javascript:void(0);" target="_blank" rel="noreferrer">
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <a href="javascript:void(0);" target="_blank" rel="noreferrer">
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
